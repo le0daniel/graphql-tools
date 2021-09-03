639b6084d879e915ab512b4caebda746e60e65fa
Leo Studer <leodaniel@nitrosoft.ch>
2021-09-03 17:10:10 +0200

    Corrected naming

--- src/Utility/SideEffects.php
+++ src/Utility/SideEffects.php
@@ -4,6 +4,15 @@ namespace GraphQlTools\Utility;
 
 class SideEffects
 {
+
+    /**
+     * Executes a callback by passing the value to it. Then returns the value regardless of what the
+     * Callback returned.
+     *
+     * @param mixed $value
+     * @param callable $callback
+     * @return mixed
+     */
     public static function tap(mixed $value, callable $callback): mixed {
         $callback($value);
         return $value;
