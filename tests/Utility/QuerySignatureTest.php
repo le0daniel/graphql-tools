84b04fd4fa0ad641cc0b45d3421ad01aac0246ac
Leo Studer <leodaniel@nitrosoft.ch>
2021-09-09 08:38:38 +0200

    Added unit tests for the query signature function

---
+++ tests/Utility/QuerySignatureTest.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types=1);
+
+namespace GraphQlTools\Test\Utility;
+
+use GraphQlTools\Utility\QuerySignature;
+use PHPUnit\Framework\TestCase;
+
+class QuerySignatureTest extends TestCase
+{
+
+    /** @dataProvider createSignatureProvider */
+    public function testCreateSignatureString(string $query, string $expected): void
+    {
+        self::assertEquals($expected, QuerySignature::createSignatureString($query));
+    }
+
+    public function createSignatureProvider(): array {
+        return [
+            'remove aliases and query' => [
+                'query {value: whoami}', '{ whoami }'
+            ],
+            'sort fields' => [
+                'query { 
+                    myType {
+                        b
+                        a
+                    }
+                }', '{ myType { a b } }'
+            ],
+            'order inline fragment' => [
+                'query {
+                    type {
+                        ... on User {
+                            id
+                        }
+                        ... on Alpha {
+                            b
+                            a
+                        }
+                    }
+                }',
+                '{ type { ... on Alpha { a b } ... on User { id } } }'
+            ],
+            'order fragments' => [
+                'query {
+                    user {
+                        ...BFragment
+                        ...AFragment
+                    }
+                }
+                
+                fragment BFragment on User {
+                    b
+                    a
+                }
+                fragment AFragment on User {
+                    b
+                    a
+                }',
+                '{ user { ...AFragment ...BFragment } } fragment AFragment on User { a b } fragment BFragment on User { a b }',
+            ],
+            'hide literals' => [
+                'query {
+                    preview(type: "small", count: 10, object: {type: "a"}, list: [{a: "string"}])
+                }',
+                '{ preview(count: 0, list: [], object: {}, type: "") }'
+            ],
+            'variables' => [
+                'query ($var1: ID!, $bvar: ID!) {
+                    preview(type: $var1, count: $bvar)
+                }',
+                'query ($bvar: ID!, $var1: ID!) { preview(count: $bvar, type: $var1) }'
+            ]
+        ];
+    }
+}
