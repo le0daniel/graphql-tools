4be0ac3102941d4b3a7edab6b812818ac015d779
Leo Studer <leodaniel@nitrosoft.ch>
2021-09-10 20:58:44 +0200

    Small name change

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@ use DG\BypassFinals;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 /**
- * By default, all non extendable classes use the final keyword. This ensures
+ * By default, all non-extendable classes use the final keyword. This ensures
  * that no such class is modified at runtime. This makes the code hard to Test
  * as we can not mock classes. Therefore, when unit tests are run, the final
  * keyword is dropped everywhere.
