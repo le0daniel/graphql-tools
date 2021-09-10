afb743768512d195fecc1cc9297e12ea6c0a8806
Leo Studer <leodaniel@nitrosoft.ch>
2021-09-10 16:55:24 +0200

    Added unit tests for Arrays.php

--- src/Utility/Arrays.php
+++ src/Utility/Arrays.php
@@ -4,21 +4,25 @@ declare(strict_types=1);
 
 namespace GraphQlTools\Utility;
 
-final class Arrays {
+final class Arrays
+{
 
-    public static function mergeKeyValue(array $array, array $arrayToPush): array {
+    public static function mergeKeyValue(array $array, array $arrayToPush): array
+    {
         foreach ($arrayToPush as $key => $value) {
             $array[$key] = $value;
         }
         return $array;
     }
 
-    public static function append(array $array, mixed $append): array {
+    public static function append(array $array, mixed $append): array
+    {
         $array[] = $append;
         return $array;
     }
 
-    public static function oneKeyExists(array $array, array $keys): bool {
+    public static function oneKeyExists(array $array, array $keys): bool
+    {
         foreach ($keys as $key) {
             if (array_key_exists($key, $array)) {
                 return true;
@@ -27,25 +31,8 @@ final class Arrays {
         return false;
     }
 
-    private static function isArrayAccessible(mixed $value): bool {
-        return is_array($value) || $value instanceof \ArrayAccess;
-    }
-
-    private static function getValueFromArray(mixed $array, string $key, mixed $defaultValue = null): mixed {
-        $keyParts = explode('.', $key);
-        $value = $array;
-
-        foreach ($keyParts as $keyPart) {
-            $value = self::isArrayAccessible($value) ? ($value[$keyPart] ?? null) : ($value->{$keyPart} ?? null);
-            if (!$value) {
-                return $defaultValue;
-            }
-        }
-
-        return $value;
-    }
-
-    public static function keysExist(array $array, array $keys): bool {
+    public static function keysExist(array $array, array $keys): bool
+    {
         foreach ($keys as $key) {
             if (!array_key_exists($key, $array)) {
                 return false;
@@ -54,23 +41,40 @@ final class Arrays {
         return true;
     }
 
-    public static function splice(array $array, int $offset, ?int $length): array {
+    private static function isArrayAccessible(mixed $value): bool
+    {
+        return is_array($value) || $value instanceof \ArrayAccess;
+    }
+
+    private static function getValueByDotNotation(mixed $array, string $key, mixed $defaultValue = null): mixed
+    {
+        $keyParts = explode('.', $key);
+        $value = $array;
+
+        foreach ($keyParts as $keyPart) {
+            $value = self::isArrayAccessible($value) ? ($value[$keyPart] ?? null) : ($value->{$keyPart} ?? null);
+            if ($value === null) {
+                return $defaultValue;
+            }
+        }
+
+        return $value ?? $defaultValue;
+    }
+
+    public static function splice(array $array, int $offset, ?int $length): array
+    {
         return array_splice($array, $offset, $length);
     }
 
-    public static function sortByColumn(array $array, string $columnKey) {
-        $sortedArray = [];
+    public static function sortByColumn(array $array, string $columnKey): array
+    {
         $columnsToSortBy = [];
 
         foreach ($array as $index => $row) {
-            $columnsToSortBy[$index] = self::getValueFromArray($row, $columnKey);
+            $columnsToSortBy[$index] = self::getValueByDotNotation($row, $columnKey);
         }
 
         array_multisort($columnsToSortBy, $array);
-
-        foreach ($columnsToSortBy as $index => $value) {
-            $sortedArray[] = $array[$index];
-        }
 
         return array_values($array);
     }
--- tests/Utility/ArraysTest.php
+++ tests/Utility/ArraysTest.php
@@ -7,9 +7,53 @@ namespace GraphQlTools\Test\Utility;
 use GraphQlTools\Utility\Arrays;
 use PHPUnit\Framework\TestCase;
 
-final class ArraysTest extends TestCase {
+final class ArraysTest extends TestCase
+{
 
-    public function testOneKeyExists(): void{
+    /** @dataProvider sortByColumnProvider */
+    public function testSortByColumn(array $array, string $column, array $expected): void
+    {
+        self::assertEquals($expected, Arrays::sortByColumn($array, $column));
+    }
+
+    public function sortByColumnProvider(): array
+    {
+        return [
+            'simple sort' => [
+                [['key' => 'v'], ['key' => 'a']],
+                'key',
+                [['key' => 'a'], ['key' => 'v']],
+            ],
+            'nested simple sort' => [
+                [['key' => ['key' => 'v']], ['key' => ['key' => 'a']]],
+                'key.key',
+                [['key' => ['key' => 'a']], ['key' => ['key' => 'v']]],
+            ]
+        ];
+    }
+
+    /** @dataProvider keysExistProvider */
+    public function testKeysExist(bool $expected, array $array, array $keys): void {
+        self::assertEquals($expected, Arrays::keysExist($array, $keys));
+    }
+
+    public function keysExistProvider(): array {
+        return [
+            'simple case' => [
+                true,
+                ['key' => 'val', 'key2' => 'val'],
+                ['key', 'key2']
+            ],
+            'invalid case' => [
+                false,
+                ['key' => 'val', 'key2' => 'val'],
+                ['key', 'key3']
+            ]
+        ];
+    }
+
+    public function testOneKeyExists(): void
+    {
         $array = ['key' => 'value', 'key2' => 'value'];
 
         self::assertTrue(Arrays::oneKeyExists($array, ['key']));
@@ -18,12 +62,14 @@ final class ArraysTest extends TestCase {
         self::assertFalse(Arrays::oneKeyExists($array, ['key3']));
     }
 
-    public function testAppend(): void{
+    public function testAppend(): void
+    {
         $array = ['test'];
         self::assertEquals(['test', 'append'], Arrays::append($array, 'append'));
     }
 
-    public function testMergeKeyValue(): void{
+    public function testMergeKeyValue(): void
+    {
         $array = ['key' => 'value'];
         self::assertEquals(
             ['key' => 'value', 'key2' => 'value'],
