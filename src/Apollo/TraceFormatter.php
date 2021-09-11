c13bfb6458d522ff95d3466ed730ba63d896e9a3
Leo Studer <leodaniel@nitrosoft.ch>
2021-09-11 14:52:24 +0200

    Added google protobuf from lighthouse-apollo

---
+++ src/Apollo/TraceFormatter.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace GraphQlTools\Apollo;
+
+use GraphQlTools\Immutable\ExecutionTrace;
+use GraphQlTools\Immutable\SchemaInformation;
+
+final class TraceFormatter
+{
+
+    public static function toProtobuf(SchemaInformation $schemaInformation, ExecutionTrace $executionTrace){
+
+    }
+
+}
