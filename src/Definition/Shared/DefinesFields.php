5c35084177ad67057434625238b59a5bcec4d882
Leo Studer <leodaniel@nitrosoft.ch>
2022-02-26 18:57:45 +0100

    Removed unused code

--- src/Definition/Shared/DefinesFields.php
+++ src/Definition/Shared/DefinesFields.php
@@ -8,7 +8,6 @@ use GraphQL\Type\Definition\FieldDefinition;
 use GraphQL\Type\Definition\Type;
 use GraphQlTools\Definition\DefinitionException;
 use GraphQlTools\Definition\Field\Argument;
-use GraphQlTools\Definition\Field\Field;
 use GraphQlTools\Definition\Field\GraphQlField;
 use GraphQlTools\Definition\Field\InputField;
 use GraphQlTools\Definition\GraphQlInterface;
@@ -31,7 +30,7 @@ trait DefinesFields
     private function initInputFields(array $inputFields): array
     {
         $initializedInputFields = [];
-        foreach ($inputFields as $name => $inputField) {
+        foreach ($inputFields as $inputField) {
             if (!$inputField) {
                 continue;
             }
@@ -60,7 +59,7 @@ trait DefinesFields
 
         /** @var GraphQlType|GraphQlInterface $this */
         $initializedFields = [];
-        foreach ($allDeclaredFields as $fieldName => $fieldDeclaration) {
+        foreach ($allDeclaredFields as $fieldDeclaration) {
             if (!$fieldDeclaration) {
                 continue;
             }
@@ -70,8 +69,7 @@ trait DefinesFields
                 throw new RuntimeException("Expected GraphQlField, got {$className}");
             }
 
-            // Attaches the field type from a given type name
-            $field = $this->createField($fieldName, $fieldDeclaration);
+            $field = $fieldDeclaration->toFieldDefinition($this->typeRepository);
             ProxyResolver::attachToField($field);
             $initializedFields[] = $field;
         }
@@ -90,52 +88,6 @@ trait DefinesFields
         }
 
         throw new DefinitionException('Could not cast type declaration to type');
-    }
-
-    private function createFieldFromString(string $className, mixed $name): FieldDefinition {
-        return FieldDefinition::create([
-            'name' => $name,
-            'type' => $this->declarationToType($className),
-            'resolve' => new ProxyResolver()
-        ]);
-    }
-
-    /**
-     * @throws DefinitionException
-     */
-    private function createField(mixed $name, mixed $fieldDeclaration): FieldDefinition
-    {
-        if ($fieldDeclaration instanceof FieldDefinition) {
-            return $fieldDeclaration;
-        }
-
-        if ($fieldDeclaration instanceof GraphQlField) {
-            return $fieldDeclaration->toFieldDefinition(
-                $this->typeRepository
-            );
-        }
-
-        if (is_string($fieldDeclaration)) {
-            return $this->createFieldFromString($fieldDeclaration, $name);
-        }
-
-        if (is_array($fieldDeclaration)) {
-            $fieldDeclaration['name'] = $name;
-            $fieldDeclaration['type'] = $this->declarationToType($fieldDeclaration['type']);
-            $fieldDeclaration['resolve'] = $fieldDeclaration['resolve'] ?? new ProxyResolver();
-            $fieldDeclaration['args'] = $this->initInputFields($fieldDeclaration['args'] ?? []);
-            return FieldDefinition::create($fieldDeclaration);
-        }
-
-        if ($fieldDeclaration instanceof Type) {
-            return FieldDefinition::create([
-                'name' => $name,
-                'type' => $fieldDeclaration,
-                'resolve' => new ProxyResolver(),
-            ]);
-        }
-
-        throw new DefinitionException('Could not create field based on your definition');
     }
 
     /**
