22b1296f4b45cc7094ca9d28d0152548e3185ff4
Leo Studer <leodaniel@nitrosoft.ch>
2022-04-03 18:17:22 +0200

    Added tests for extensions

--- tests/Execution/ExtensionsTest.php
+++ tests/Execution/ExtensionsTest.php
@@ -2,14 +2,19 @@
 
 namespace GraphQlTools\Test\Execution;
 
+use GraphQL\Executor\ExecutionResult;
 use GraphQlTools\Contract\Extension;
+use GraphQlTools\Events\EndEvent;
+use GraphQlTools\Events\StartEvent;
 use GraphQlTools\Events\VisitFieldEvent;
+use GraphQlTools\Helper\Extension\FieldMessages;
 use GraphQlTools\Helper\Extensions;
 use GraphQlTools\Helper\Extension\Tracing;
 use GraphQlTools\Test\Dummies\ResolveInfoDummy;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\ObjectProphecy;
 
 class ExtensionsTest extends TestCase
 {
@@ -22,6 +27,30 @@ class ExtensionsTest extends TestCase
             fn() => new Tracing()
         ]);
         self::assertTrue(true);
+    }
+
+    public function testExtensionValidationRules() {
+        self::assertEmpty((new Extensions())->collectValidationRules());
+        self::assertCount(1, (new Extensions(new FieldMessages()))->collectValidationRules());
+    }
+
+    public function testExtensionsEventDispatching() {
+        $startEvent = StartEvent::create('');
+        $endEvent = EndEvent::create(new ExecutionResult(null));
+
+        /** @var Extension|ObjectProphecy $extensionProphecy */
+        $extensions = [];
+
+        for ($i = 0; $i < 2; $i++) {
+            $extensionProphecy = $this->prophesize(Extension::class);
+            $extensionProphecy->start($startEvent)->shouldBeCalledOnce();
+            $extensionProphecy->end($endEvent)->shouldBeCalledOnce();
+            $extensions[] = $extensionProphecy->reveal();
+        }
+
+        $extensionManager = new Extensions(...$extensions);
+        $extensionManager->dispatchStartEvent($startEvent);
+        $extensionManager->dispatchEndEvent($endEvent);
     }
 
     public function testMiddlewareFieldResolution()
