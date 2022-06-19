b964dacc2e63d26b0ecbd0b9fe81719916f850b3
Leo Studer <leodaniel@nitrosoft.ch>
2022-06-19 23:26:47 +0200

    Removed.

--- src/Utility/Time.php
+++ src/Utility/Time.php
@@ -8,11 +8,13 @@ namespace GraphQlTools\Utility;
 
 final class Time
 {
+    private const DEFAULT_PRECISION = 6;
+
     public static function nanoSeconds(): int {
         return hrtime(true);
     }
 
-    public static function nanoSecondsToSeconds(int $nanoSeconds): float {
-        return $nanoSeconds / (1000 * 1000 * 1000);
+    public static function nanoSecondsToSeconds(int $nanoSeconds, int $precision = self::DEFAULT_PRECISION): float {
+        return round($nanoSeconds / (1000 * 1000 * 1000), $precision);
     }
 }
