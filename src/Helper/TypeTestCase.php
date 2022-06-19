ba39876ad5c6d622e41e02faf43cd05622eb509c
Leo Studer <leodaniel@nitrosoft.ch>
2022-06-19 23:28:25 +0200

    Formatting

--- src/Helper/TypeTestCase.php
+++ src/Helper/TypeTestCase.php
@@ -2,22 +2,14 @@
 
 namespace GraphQlTools\Helper;
 
-use GraphQL\Executor\Promise\Adapter\SyncPromise;
-use GraphQL\Type\Definition\ResolveInfo;
-use GraphQlTools\Context;
-use GraphQlTools\Definition\Field\GraphQlField;
-use GraphQlTools\Test\Dummies\ResolveInfoDummy;
 use PHPUnit\Framework\TestCase;
-use ReflectionClass;
-use ReflectionObject;
-use RuntimeException;
-use Throwable;
 
 abstract class TypeTestCase extends TestCase
 {
     abstract protected function typeClassName(): string;
 
-    final protected function field(string $name): FieldTestCase {
+    final protected function field(string $name): FieldTestCase
+    {
         return new FieldTestCase($this->typeClassName(), $name);
     }
 
