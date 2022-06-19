f0acb9ee03f7f2418100420ebcba1febf1bad098
Leo Studer <leodaniel@nitrosoft.ch>
2022-06-19 23:23:00 +0200

    Better validation rule concept with contextual validation rules

---
+++ src/Contract/ContextualValidationRule.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace GraphQlTools\Contract;
+
+use GraphQL\Validator\Rules\ValidationRule;
+use JsonSerializable;
+
+abstract class ContextualValidationRule extends ValidationRule implements JsonSerializable
+{
+
+    /**
+     * Validation rules are able to show data in the result of the query under
+     * the extensions field. This is the key under which data is potentially exposed.
+     *
+     * @return string
+     */
+    abstract public function key(): string;
+
+    /**
+     * Determines if the data should be exposed in the result.
+     *
+     * @return bool
+     */
+    abstract public function isVisibleInResult(): bool;
+
+}
