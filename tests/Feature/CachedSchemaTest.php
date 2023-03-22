9783430170c2e756fdb1c8c3a92c538b46246fe6
Leo Studer <leodaniel@nitrosoft.ch>
2023-03-23 00:19:50 +0100

    Simplified and refactored compiling

--- tests/Feature/CachedSchemaTest.php
+++ tests/Feature/CachedSchemaTest.php
@@ -13,32 +13,6 @@ class CachedSchemaTest extends QueryTest
     {
         $code = $this->federatedSchema()->cacheSchema();
 
-        $federatedSchema->extendType(
-            UserType::class,
-            fn(TypeRegistry $registry) => [
-                Field::withName('extended')
-                    ->ofType(Type::string())
-                    ->resolvedBy(fn() => 'extended'),
-                Field::withName('closure')
-                    ->ofType($registry->type(JsonScalar::class))
-                    ->resolvedBy(fn() => 'closure')
-            ],
-        );
-
-        $federatedSchema->extendType('User',fn(TypeRegistry $registry) => [
-            Field::withName('byName')
-                ->ofType(Type::string())
-                ->resolvedBy(fn() => 'byName'),
-
-            // Ensure circular dependencies work fine
-            Field::withName('testCircular')
-                ->ofType($registry->type('User')),
-
-            'lazy' => fn() => Field::withName('lazy')
-                ->ofType(Type::string())
-                ->resolvedBy(fn() => 'lazy-field')
-        ]);
-        $code = $federatedSchema->cacheSchema();
         $schema = FederatedSchema::fromCachedSchema(
             eval($code),
             'Query'
