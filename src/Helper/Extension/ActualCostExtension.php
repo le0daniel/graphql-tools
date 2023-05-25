145dece2686c84bd5f7ea1e3b516a8a5e2a85e18
Leo Studer <leodaniel@nitrosoft.ch>
2023-05-25 11:19:32 +0200

    Added a minimum cost for every query, no matter the actual cost

--- src/Helper/Extension/ActualCostExtension.php
+++ src/Helper/Extension/ActualCostExtension.php
@@ -3,10 +3,19 @@
 namespace GraphQlTools\Helper\Extension;
 
 use Closure;
+use GraphQlTools\Events\StartEvent;
 use GraphQlTools\Events\VisitFieldEvent;
 
 final class ActualCostExtension extends Extension
 {
+    private const DEFAULT_MIN_QUERY_COST = 2;
+
+    public function __construct(
+        private readonly int $minQueryCost = self::DEFAULT_MIN_QUERY_COST,
+    )
+    {
+    }
+
     private int $usedCost = 0;
 
     public function key(): string
@@ -20,8 +29,12 @@ final class ActualCostExtension extends Extension
         return null;
     }
 
-    public function jsonSerialize(): mixed
+    public function getCost(): int {
+        return max($this->usedCost, $this->minQueryCost);
+    }
+
+    public function jsonSerialize(): int
     {
-        return $this->usedCost;
+        return $this->getCost();
     }
 }
